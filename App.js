3a0fa3de920554544da363cc899a24c111827bd9
Simon Ferlat <ferlat.simon@gmail.com>
2020-04-04 12:14:04 +0200

    galere du début

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-import CustomCount from './Count.js'
-import {Buttons} from './Buttons.js'; 
+import Count from './Count.js'
 
 //check video at 42 min: https://www.youtube.com/watch?v=Gk6RF5k3C2M&feature=youtu.be
 
@@ -11,8 +10,7 @@ export default class App extends React.Component {
     return (
 
       <View style={styles.container}>
-       <View><Buttons></Buttons></View>
-		<CustomCount count={0} />
+		<Count>count</Count>
 		</View>
     );
   }
--- Count.js
+++ Count.js
@@ -1,23 +1,80 @@
 import React from 'react'
-import {StyleSheet, Text} from 'react-native'
+import {StyleSheet, Text, View, Button} from 'react-native'
 import PropTypes from 'prop-types'
 
 const styles = StyleSheet.create({
   text: {fontSize: 72},
 })
 
+
 class Count extends React.Component {
-  static propTypes = {
-    count: PropTypes.number.isRequired,
+  constructor(props) {
+    super(props)
+    this.state = {
+      count: 0,
+	  running: 'Y',
+    }
   }
 
   render() {
     return (
+	
+	<View>
+	<Button title="Start"   /*onPress={() => {
+    this.start();
+  }}*/>
+  </Button>
+	<Button title="Stop" onPress={() => {
+    this.stop();
+  }}></Button>	
+  	<Button title="Reset" onPress={() => {
+    this.resetme();
+  }}></Button>	
       <Text style={styles.text}>
-        {this.props.count}
+		  {this.state.count}
       </Text>
+	  </View>
     )
   }
+
+
+  componentDidMount() {
+	  if(this.state.running === 'Y' ){
+		  this.interval = setInterval(this.incrementCount, 1000)
+	  } else {
+			alert ('what')
+	  }
+  }
+  
+componentDidUpdate(prevProps, prevState) {
+  if (prevState.running !== this.state.running) {
+alert ('please stop the counter')  
 }
+
+	if(this.state.count === 25){
+			alert('you should have a break now')
+	}
+}
+  
+  
+  incrementCount = () => {
+    this.setState(prevState => ({count: prevState.count + 1}))
+  }
+
+  stop = () => {
+	this.setState(prevState => ({count: 0}))
+    this.setState(state => ({running: 'N'}))
+  }
+  
+    resetme = () => {
+	this.setState(prevState => ({count: 0}))
+  }
+  
+    componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  
+}
+
 
 export default Count
